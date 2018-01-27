8ad7e37c0a11005d433aeb9ab1921658f9afa83b
Marie Zamzhitskaya <marie@keezel.co>
2018-01-27 21:39:07 +0100

    Delete service worker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './modules/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-registerServiceWorker();
